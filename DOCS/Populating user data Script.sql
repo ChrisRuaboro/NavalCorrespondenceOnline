fefa73537e0f469b451a05efca29895865038b8e
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2019-09-11 21:51:36 -0700

    Add files via upload

---
+++ DOCS/Populating user data Script.sql
@@ -0,0 +1,202 @@
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (1,'Rodriguez','Hannah', 'O', 9);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (2,'Morgan','Ian', 'GS', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (3,'Peterson','Aiden', 'E', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (4,'Flores','Hunter', 'O', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (5,'Baker','Oliver', 'WG', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (6,'Kelly','Joshua', 'E', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (7,'Lee','Jake', 'O', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (8,'Butler','Jacob', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (9,'Torres','Sebastian', 'WG', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (10,'Reed','Madelyn', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (11,'Peterson','Joshua', 'E', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (12,'Henderson','Aiden', 'WG', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (13,'Sanders','Ethan', 'WG', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (14,'Wright','Julian', 'GS', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (15,'Torres','Charlie', 'WG', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (16,'Rivera','Julia', 'O', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (17,'Collins','Austin', 'O', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (18,'Garcia','Wyatt', 'WG', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (19,'Green','Nora', 'GS', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (20,'Peterson','Grace', 'WG', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (21,'Watson','Claire', 'E', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (22,'Edwards','Abby', 'E', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (23,'Clark','Chloe', 'WG', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (24,'Torres','Jayden', 'GS', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (25,'Thompson','Julian', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (26,'Williams','Benjamin', 'WG', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (27,'Henderson','Julia', 'O', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (28,'Ward','Cameron', 'E', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (29,'Sanders','Hunter', 'GS', 9);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (30,'Torres','Piper', 'WG', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (31,'Garcia','Chloe', 'E', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (32,'Thomas','Jace', 'WG', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (33,'Thompson','Lucas', 'GS', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (34,'Edwards','Makayla', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (35,'Jones','Eli', 'O', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (36,'Coleman','Cameron', 'O', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (37,'Bell','Allison', 'GS', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (38,'Bryant','Matthew', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (39,'James','Layla', 'GS', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (40,'Gonzales','Nathan', 'WG', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (41,'Jenkins','Nolan', 'GS', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (42,'Gonzalez','Ava', 'E', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (43,'Collins','Ethan', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (44,'Murphy','Sophia', 'GS', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (45,'Hayes','Taylor', 'WG', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (46,'Barnes','Ella', 'E', 9);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (47,'Russell','Evan', 'O', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (48,'Martin','Aaliyah', 'E', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (49,'Anderson','Alexandra', 'WG', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (50,'Jones','Levi', 'E', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (51,'Kelly','Jasmine', 'E', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (52,'Watson','Tristan', 'GS', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (53,'Diaz','Jace', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (54,'Bell','Adrian', 'O', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (55,'Hughes','Max', 'O', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (56,'Torres','Tristan', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (57,'Hill','Mackenzie', 'E', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (58,'Young','Kendall', 'WG', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (59,'Price','Eva', 'O', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (60,'Miller','Jocelyn', 'E', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (61,'Mitchell','Henry', 'E', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (62,'Griffin','Gabriella', 'E', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (63,'Cooper','Allison', 'WG', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (64,'Harris','Colton', 'WG', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (65,'Long','Scarlett', 'E', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (66,'Rogers','Andrew', 'GS', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (67,'Lewis','Alexa', 'GS', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (68,'Davis','Annabelle', 'O', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (69,'Ramirez','Kylie', 'E', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (70,'Scott','Maria', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (71,'Morgan','Hayden', 'E', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (72,'Howard','William', 'O', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (73,'Hill','Mason', 'GS', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (74,'Diaz','Brianna', 'GS', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (75,'Powell','Piper', 'E', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (76,'King','Kate', 'E', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (77,'Coleman','Brody', 'WG', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (78,'Walker','Brooke', 'O', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (79,'Martinez','Lucas', 'E', 9);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (80,'Evans','Nathaniel', 'GS', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (81,'Lopez','Nevaeh', 'O', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (82,'Moore','Alexa', 'GS', 2);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (83,'Bell','Justin', 'E', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (84,'Gonzalez','Grayson', 'O', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (85,'Brown','Ashlyn', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (86,'Reed','Aaliyah', 'E', 7);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (87,'Lopez','Brayden', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (88,'Peterson','Nora', 'O', 3);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (89,'Rodriguez','Connor', 'GS', 9);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (90,'Ross','Mia', 'O', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (91,'Lewis','Chloe', 'GS', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (92,'Gonzalez','Aaron', 'GS', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (93,'Barnes','Arianna', 'GS', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (94,'Hayes','Kennedy', 'GS', 1);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (95,'Foster','Ava', 'O', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (96,'Kelly','Mason', 'E', 4);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (97,'Green','Cameron', 'WG', 6);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (98,'Henderson','Connor', 'O', 5);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (99,'Sanders','Wyatt', 'O', 8);
+Insert into Users(UserID, LastName, FirstName, PayGradeType, PayGradeRank)
+Values (100,'Patterson','Maria', 'WG', 4);
+
+
